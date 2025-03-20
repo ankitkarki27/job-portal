fb1856a8500f767838341bd5dbc9bb0357046339
ankitkarki15 <ankitkarki8088@gmail.com>
2025-03-21 02:55:45 +0545

    edit on job portal's landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
                         Join thousands of job seekers and top companies on the most reliable platform for opportunities.
                     </p>
                     
-                    <div class="mt-12 flex flex-wrap gap-4 justify-left md:justify-start">
+                    <div class="mt-8 flex flex-wrap gap-4 justify-left md:justify-start">
                         {{-- <a href="{{ route('login') }}" class="text-blue-500 hover:underline">Login</a> --}}
                         <button class="bg-gray-900 hover:bg-gray-900 text-white font-semibold px-6 py-3 rounded-lg">
                            Find Jobs
